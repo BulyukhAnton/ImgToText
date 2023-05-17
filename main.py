8599b80862a5832fb2b90de1da7655b77889b2ea
BulyukhAnton <98349628+BulyukhAnton@users.noreply.github.com>
2023-05-17 22:27:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,23 @@
 
 from PIL import Image
 
-im = Image.open("img.jpg")
-im = im.convert("1")
-im.save("img.jpg")
-image = Image.open("img.jpg")
+im = Image.open('img.jpg')
+im = im.convert('1')
+im.save('img.jpg')
+image = Image.open('img.jpg')
 width, height = image.size
 counter = 0
 
 
-for px in image.getdata():
-    if px == 255:
-        print(" ", end="")
-    else:
-        print("@", end="")
+
+for px in image.getdata(): # getting all the pi
+
+
+    if px >= 127: # if pixel is light it must be white
+        print(' ', end='')
+    else: # else it must be dark
+        print("@", end='')
     counter += 1
-
-    if counter == width:  # when the line ended
+    if counter == width:
         counter = 0
-        print("\n", end="")
-
-
+        print('\n', end='')
