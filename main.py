497bd8bd50080cc7609cb2414883f270bef82d5b
BulyukhAnton <98349628+BulyukhAnton@users.noreply.github.com>
2023-05-17 14:44:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+# This is fun cod. It's my first project so i hope you like it.
+
+from PIL import Image
+
+im = Image.open("img.jpg")
+im = im.convert("1")
+im.save("img.jpg")
+image = Image.open("img.jpg")
+width, height = image.size
+counter = 0
+
+
+for px in image.getdata():
+    if px == 255:
+        print(" ", end="")
+    else:
+        print("@", end="")
+    counter += 1
+
+    if counter == width:  # when the line ended
+        counter = 0
+        print("\n", end="")
+
 
